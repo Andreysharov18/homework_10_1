f3ea6d7b309635a152a2cf9d48c61f042115aef4
Шаров Андрей <sarovandrej@gmail.com>
2025-02-19 12:03:33 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number
 
 
 def mask_account_card(data_card: str) -> str:
