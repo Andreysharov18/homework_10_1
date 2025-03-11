fc52908aad50cd6f4afa8a710f78ea3319f1b41d
Шаров Андрей <sarovandrej@gmail.com>
2025-03-12 00:34:36 +0300

    дороботка функции

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,19 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номер карты"""
 
+    if len(card_number) < 16:
+        raise ValueError("Номер карты должен содержать 16 цифр.")
+
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция, которая маскирует номер счета"""
 
-    return f"**{account_number[-4:]}"
+    if len(account_number) < 8:  # Предполагается, что номера счетов должны быть не менее 8 цифр
+        raise ValueError("Номер счета должен содержать минимум 8 цифр.")
+
+    return f"{'*' * (len(account_number) - 4)}{account_number[-4:]}"
 
 
 print(get_mask_card_number("7000792289606361"))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -13,6 +13,11 @@ def test_get_mask_card_number(card_number):
   assert masked_number == "1234 56** **** 5678"
 
 
+import pytest
 
-
+@pytest.fixture
+def test_get_mask_account():
+    account_number = '73654108430135874305'
+    masked_account = get_mask_account(account_number)
+    assert masked_account == "****************4305"
 
